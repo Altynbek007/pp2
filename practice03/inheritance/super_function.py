eff65b8709c3101c8b57d36150ad01527acdddf4
Altynbek <altynbektazybaj626@gmail.com>
2026-02-21 10:37:56 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

--- practice03/inheritance/super_function.py
+++ practice03/inheritance/super_function.py
@@ -1,3 +1,12 @@
-class Student(Person):
-  def __init__(self, fname, lname):
-    super().__init__(fname, lname)
+class Parent:
+    def __init__(self, name):
+        self.name = name
+
+
+class Child(Parent):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+c = Child("Anna")
+print(c.name)

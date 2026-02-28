0458b4ad4828926b1f3f89fcc0040313494e819a
Altynbek <altynbektazybaj626@gmail.com>
2026-02-28 11:18:10 +0500

    Add Practice4 - Python iterators, generators, dates, math, and JSON

--- pactice04/its_json.py
+++ pactice04/its_json.py
@@ -24,3 +24,34 @@ data = '["apple", "banana", "cherry"]'
 parsed = json.loads(data)
 
 print(type(parsed))
+
+#4
+import json
+
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+
+#5
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x))

27dd007d4c09129e23659edcbf46fa03dcb5f9fe
Altynbek <altynbektazybaj626@gmail.com>
2026-03-28 10:55:16 +0500

    Add Practice7 - PhoneBook with PostgreSQL

---
+++ practice07/config.py
@@ -0,0 +1,8 @@
+def load_config():
+    return {
+        "host": "localhost",
+        "database": "phonebook_db",
+        "user": "postgres",
+        "password": "221177",
+        "port": 5432
+    }

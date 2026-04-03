51b5564b83c27ff6a8ede49d0dd3689d9fa1a2b7
Altynbek <altynbektazybaj626@gmail.com>
2026-04-04 00:13:53 +0500

    Add Practice8 - PhoneBook with functions and stored procedures

---
+++ practice08/config.py
@@ -0,0 +1,8 @@
+def load_config():
+    return {
+        "host": "localhost",
+        "database": "postgres",
+        "user": "postgres",
+        "password": "221177",
+        "port": 5432
+    }
